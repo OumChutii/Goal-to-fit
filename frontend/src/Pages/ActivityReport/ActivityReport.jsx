d2406a26c3b2dd285c6dd616320f734be017eba3
chutikan prasert <chutikan.prasert@gmail.com>
2022-04-20 22:58:49 +0700

    up state

--- frontend/src/Pages/ActivityReport/ActivityReport.jsx
+++ frontend/src/Pages/ActivityReport/ActivityReport.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useState,useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
 import './ActivityReport.css';
 import Footer from '../../components/Footer/Footer';
