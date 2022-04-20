039f3474b194b574e499e28d3e20f707b6b40827
chutikan prasert <chutikan.prasert@gmail.com>
2022-04-20 23:41:42 +0700

    up client

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const client = axios.create({
-  baseURL: "http://localhost:4000",
+  baseURL: "https://goal-to-9cwdft49s-oumchutii.vercel.app/",
 });
 
 export const getRecords = async () =>{
